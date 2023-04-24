b8d16a4ea3ad1d88f6eb63def055b640d81feaf5
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-24 09:59:16 -0300

    :sparkles: update: page - dashboard (.../src/pages)

--- test-demo/src/pages/dashboard/index.tsx
+++ test-demo/src/pages/dashboard/index.tsx
@@ -1,7 +1,17 @@
 export default function DashboardIndexPage() {
   return(
     <div>
-      <h1>Hello World</h1>
+      
+      <h1>Hello World :D</h1>
+      
+      <button disabled={true}>
+        Click :D
+      </button>
+
+      <p data-testid="paragraph-blue" className="blue">
+        {"Pedro <:()"}
+      </p>
+
     </div>
   )
 }
